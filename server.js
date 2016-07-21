890488bad0284f76ef3947b3ec89c7319e44b14b
Ishtiaq hussain <leedsuk001@yahoo.co.uk>
2016-07-21 11:28:06 -0700

    Changed interpolation to render mongoose in th view properly

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express  = require( 'express' ),
     path     = require( 'path' ),
     root     = __dirname,
-    port     = process.env.PORT || 8000,
+    port     = process.env.PORT || 5000,
     bp       = require('body-parser'),
     app      = express();
 
 
 app.use( express.static( path.join( root, 'client' )));
 app.use( express.static( path.join( root, 'bower_components' )));
-app.use(bp.json())
+app.use(bp.json());
 
 require('./server/config/db.js');
 require('./server/config/routes.js')(app);
